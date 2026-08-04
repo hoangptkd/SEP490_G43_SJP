71597c99d7cb71dd5114ef9da242087b77ba9bbf
HoangNV <hoangptkdhy@gmail.com>
2026-08-04 14:17:39 +0700

    fix database

--- backend/src/main/resources/db/migration/V25__drop_ai_ranking_trigger.sql
+++
@@ -1,3 +0,0 @@
--- Drop the old AI ranking trigger and function that checked ranking_job_id
-DROP TRIGGER IF EXISTS ai_ranking_results_check_job ON ai_ranking_results;
-DROP FUNCTION IF EXISTS check_ai_ranking_result_job();
